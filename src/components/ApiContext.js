01fb585ca248a2ab597af6d08f421d537e3aff1f
Tanuj1021 <54186010+Tanuj1021@users.noreply.github.com>
2023-11-15 12:19:50 +0530

    Second Commit

--- src/components/ApiContext.js
+++ src/components/ApiContext.js
@@ -1,7 +1,7 @@
-import React, { createContext, useState, useEffect } from 'react';
+import React, { createContext, useState, useEffect } from 'react'
 import axios from 'axios';
 
-export const ApiContext = createContext();
+export const ApiContext = createContext()
 
 export const ApiProvider = ({ children }) => {
     const [products, setProducts] = useState([]);
@@ -13,7 +13,6 @@ export const ApiProvider = ({ children }) => {
             const url = "https://fakestoreapi.com/products"
             const response = await axios.get(url)
             setProducts(response.data)
-            console.log(products[0].title)
         } catch (error) {
             console.error('Error fetching API data', error)
         }
@@ -21,17 +20,37 @@ export const ApiProvider = ({ children }) => {
 
     useEffect(() => {
         fetchData()
-    }, []);
+    }, [])
 
     const addToCart = (product) => {
-        setCart([...cart, product]);
+        const existingProductIndex = cart.findIndex(item => item.id === product.id)
+
+        if (existingProductIndex !== -1) {
+            const updatedCart = [...cart]
+            updatedCart[existingProductIndex].quantity += 1
+            setCart(updatedCart);
+        } else {
+            setCart([...cart, { ...product, quantity: 1 }])
+        }
+
         setPrice(price + product.price)
     };
 
     const removeFromCart = (productId, productPrice) => {
-        setCart(cart.filter(item => item.id !== productId))
-        setPrice(price - productPrice)
-    };
+        const existingProduct = cart.find(item => item.id === productId)
+
+        if (existingProduct && existingProduct.quantity > 1) {
+            const updatedCart = cart.map(item =>
+                item.id === productId ? { ...item, quantity: item.quantity - 1 } : item
+            )
+            setCart(updatedCart)
+        } else {
+            const updatedCart = cart.filter(item => item.id !== productId);
+            setCart(updatedCart)
+        }
+
+        setPrice(price - productPrice);
+    }
 
     return (
         <ApiContext.Provider value={{ products, cart, price, addToCart, removeFromCart }}>
--- src/screens/Cart.js
+++ src/screens/Cart.js
@@ -1,4 +1,4 @@
-import { View, Text, ScrollView, Image, TouchableOpacity, FlatList } from 'react-native'
+import { View, Text, ScrollView, TouchableOpacity, FlatList, ActivityIndicator } from 'react-native'
 import React, { useState, useContext } from 'react'
 import Header from '../components/Header'
 import AsyncStorage from '@react-native-async-storage/async-storage'
@@ -7,11 +7,18 @@ import CartCard from '../components/CartCard'
 
 const Cart = ({ navigation }) => {
 
-    const { cart, price, removeFromCart } = useContext(ApiContext)
-    const [counter, setCounter] = useState(1)
+    const { cart, price, removeFromCart, addToCart } = useContext(ApiContext)
     const [disable, setDisable] = useState(false)
     const [subTotal, setSubTotal] = useState(price)
     const [total, setTotal] = useState(subTotal + 8)
+
+    const handleIncrement = (item) => {
+        addToCart(item)
+    }
+
+    const handleDecrement = (item) => {
+        removeFromCart(item.id, item.price)
+    }
 
     return (
         <View style={{ flex: 1, backgroundColor: '#002e65', paddingHorizontal: 16 }}>
@@ -25,71 +32,69 @@ const Cart = ({ navigation }) => {
             />
             <View style={{ flex: 1, justifyContent: 'space-between' }}>
                 <ScrollView>
-                    <FlatList
-                        data={cart}
-                        keyExtractor={item => item.id}
-                        renderItem={({ item }) => {
-                            return (
-                                <CartCard
-                                    SRC={item.image}
-                                    itemTitle={item.title}
-                                    itemPrice={item.price}
-                                    itemCount={item.rating.count}
-                                    itemId={item.id}
-                                    onPressMinus={() => {
-                                        if (counter === 1) {
-                                            removeFromCart(item.id)
-                                            setCounter(counter - 1)
-                                            // setDisable(true)
-                                        }
-                                        else {
-                                            setCounter(counter - 1)
-                                        }
-                                        setSubTotal(subTotal - item.price)
-                                        setTotal(total - item.price)
-                                    }}
-                                    counter={counter}
-                                    onPressPlus={() => {
-                                        if (counter === item.rating.count) {
-                                            setDisable(true)
-                                        } else {
-                                            setCounter(counter + 1)
-                                            setSubTotal(subTotal + item.price)
-                                            setTotal(total + item.price)
-                                        }
-                                    }}
-                                />
-                            )
-                        }}
-                    />
+                    {
+                        cart.length === 0 ?
+                            <View style={{ backgroundColor:'white', paddingVertical: 16, margin: 16, justifyContent:'center', alignItems:'center', borderRadius: 8}}>
+                                <ActivityIndicator size={'large'} color={'black'}  />
+                                <Text style={{ fontSize: 20, fontWeight: 500, color: 'black' }}>
+                                    Your cart is empty!
+                                </Text>
+                            </View> :
+                            <FlatList
+                                data={cart}
+                                keyExtractor={item => item.id}
+                                renderItem={({ item }) => {
+                                    return (
+                                        <CartCard
+                                            SRC={item.image}
+                                            itemTitle={item.title}
+                                            itemPrice={item.price}
+                                            itemCount={item.rating.count}
+                                            itemId={item.id}
+                                            onPressMinus={() => handleDecrement(item)}
+                                            counter={item.quantity}
+                                            onPressPlus={() => handleIncrement(item)}
+                                        />
+                                    )
+                                }}
+                            />
+                    }
                 </ScrollView>
                 <View>
                     <View style={{ borderWidth: 4, borderColor: 'white', padding: 8 }}>
                         <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
                             <Text style={{ fontSize: 20, fontWeight: 500, color: 'white' }}>
-                                Subtotal :
-                            </Text>
-                            <Text style={{ fontSize: 20, fontWeight: 500, color: 'white' }}>
-                                $ {subTotal}
-                            </Text>
-                        </View>
-                        <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 4 }}>
-                            <Text style={{ fontSize: 20, fontWeight: 500, color: 'white' }}>
-                                Tax (GST) :
-                            </Text>
-                            <Text style={{ fontSize: 20, fontWeight: 500, color: 'white' }}>
-                                $ 8
-                            </Text>
-                        </View>
-                        <View style={{ height: 2, backgroundColor: 'white', marginVertical: 4 }} />
-                        <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-                            <Text style={{ fontSize: 20, fontWeight: 500, color: 'white' }}>
                                 Total :
                             </Text>
                             <Text style={{ fontSize: 20, fontWeight: 500, color: 'white' }}>
-                                $ {total}
+                                $ {price}
                             </Text>
                         </View>
+                    </View>
+                    <View style={{ flexDirection: 'row', marginVertical: 8 }}>
+                        <TouchableOpacity
+                            style={{
+                                paddingVertical: 8, width: '50%', justifyContent: 'center', alignItems: 'center', backgroundColor: 'white', borderRadius: 4, marginRight: 4
+                            }}
+                            onPress={() => navigation.navigate('Home')}
+                        >
+                            <Text style={{ fontSize: 16, fontWeight: 500, color: 'black' }}>
+                                Shop More
+                            </Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity
+                            style={{
+                                paddingVertical: 8, width: '50%', justifyContent: 'center', alignItems: 'center', backgroundColor: 'orange', borderRadius: 4
+                            }}
+                            onPress={() => {
+                                Alert("Purchase Successful")
+                                navigation.navigate('Login')
+                            }}
+                        >
+                            <Text style={{ fontSize: 16, fontWeight: 500, color: 'white' }}>
+                                Buy Now
+                            </Text>
+                        </TouchableOpacity>
                     </View>
                 </View>
             </View>
