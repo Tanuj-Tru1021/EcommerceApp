9fee90e7b51a4d0eb1f988bcb8a22ac83c45ce5b
TRUWORTHWELLNES\tanuj.saxena <tanuj.saxena@truworthwellnes.com>
2023-11-22 16:53:56 +0530

    Changes in splash screen.

--- src/screens/Splash.js
+++ src/screens/Splash.js
@@ -1,31 +1,44 @@
 import { View, Image } from 'react-native'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 import Logo from '../../assets/logo.png'
 
 const Splash = ({ navigation }) => {
+
+    const [flag, setFlag] = useState()
     const handle = async () => {
         const mEmail = await AsyncStorage.getItem('Email')
         const mPassword = await AsyncStorage.getItem('Password')
         !mEmail && !mPassword ?
             navigation.navigate('Login')
             : navigation.navigate('Home')
+        await AsyncStorage.setItem("token", "false")
+    }
+
+    const setToken = async () => {
+        await AsyncStorage.setItem("token", "true")
+        setFlag(await AsyncStorage.getItem("token"))
     }
     useEffect(() => {
+        setToken()
         setTimeout(async () => {
             handle()
         }, 3000)
     })
     return (
-        <SafeAreaProvider>
-            <View style={{ backgroundColor: 'white', flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                <Image
-                    source={Logo}
-                    style={{ height: 400, width: 400 }}
-                />
-            </View>
-        </SafeAreaProvider>
+        <>
+            {
+                flag && <SafeAreaProvider>
+                    <View style={{ backgroundColor: 'white', flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                        <Image
+                            source={Logo}
+                            style={{ height: 400, width: 400 }}
+                        />
+                    </View>
+                </SafeAreaProvider>
+            }
+        </>
     )
 }
 
